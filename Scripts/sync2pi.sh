2ac0ffc0e3f28b713dc877d627f8b998ad6cee05
omo <mingledmoxu@gmail.com>
2026-05-25 13:26:23 +0800

    主工程重构

--- Scripts/sync2pi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -avz --cvs-exclude ../../RpiANC --exclude "RpiANC/cmake-build-debug" --exclude "RpiANC/cmake-build-release" --exclude "RpiANC/Scripts/audio_files" --exclude "RpiANC/Makefile" --exclude "RpiANC/OctaveCode" --exclude "RpiANC/.idea" ../../RpiANC pi@raspberry:./
