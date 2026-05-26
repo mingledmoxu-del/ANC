f6770e280fbf4c9e1c9cec16bf9992304f1b545d
mingledmoxu-del <mingledmoxu@gmail.com>
2026-05-26 10:58:50 +0800

    win环境开发

---
+++ src/fir_demo.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: mingledmoxu-del mingledmoxu@gmail.com
+ * @Date: 2026-05-26 10:43:44
+ * @LastEditors: mingledmoxu-del mingledmoxu@gmail.com
+ * @LastEditTime: 2026-05-26 10:58:02
+ * @FilePath: \anc\src\fir_demo.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
+ * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Output format: [Index], [Original], [Filtered]\n");
+
+    return 0;
+}
